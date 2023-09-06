3bfd52bb7fa65adfb6cf30622b7fbd226f15e96f
Andrew Briden <agbriden@gmail.com>
2023-09-06 19:51:10 -0400

    Initial Commit

---
+++ src/main/java/edu/Sample.java
@@ -0,0 +1,11 @@
+package edu;
+
+public class Sample {
+    public double add(double arg1, double arg2){
+        return arg1 + arg2;
+    }
+
+    public double multiply(double arg1, double arg2){
+        return arg1*arg2;
+    }
+}
---
+++ src/test/java/edu/SampleTest.java
@@ -0,0 +1,16 @@
+package edu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+}
